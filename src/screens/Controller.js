050e1741e7b211d7f49a508949041b744894dd3e
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-10-06 13:05:22 +0530

    pre setup for header component and router configuration

---
+++ src/screens/Controller.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../screens/home/Home";
+
+class Controller extends Component {
+  constructor() {
+    super();
+    this.baseUrl = "http://localhost:8080/api/v1/";
+    this.state = {
+      loggedIn: sessionStorage.getItem("access-token") == null ? false : true,
+    };
+  }
+  render() {
+    return (
+      <Router>
+        <div className="main-container">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => <Home {...props} baseUrl={this.baseUrl} />}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default Controller;

b0211f431902ad9299ed1a537cfb16af345d7cdb
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-10-06 14:04:46 +0530

    login button added to header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./Header.css";
 import Fastfood from "@material-ui/icons/Fastfood";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Button from "@material-ui/core/Button";
 
 class Header extends Component {
   constructor(props) {
@@ -14,6 +16,12 @@ class Header extends Component {
           <div className="flex-container-header">
             <div className="app-logo">
               <Fastfood style={{ fontSize: "35px", color: "white" }} />
+            </div>
+            <div className="app-login">
+              <Button variant="contained" color="default">
+                <AccountCircle style={{ marginRight: 4 }} />
+                LOGIN
+              </Button>
             </div>
           </div>
         </header>

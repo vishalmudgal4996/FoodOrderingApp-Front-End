9f2a12801027804b6327156b5597696c59339521
NutanSH <hegdenutan92@gmail.com>
2020-10-10 03:01:19 +0530

    Add api call to restaurant controller

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,13 +1,44 @@
-import React, { Component } from "react";
-import Header from "./../../common/header/Header";
+import React, { Component } from 'react';
+import './Home.css';
+import Header from '../../common/header/Header';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import CardActionArea from '@material-ui/core/CardActionArea';
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-  }
+    this.state = {
+        restaurantList: []
+    }
+}
 
-  updateSearchRestaurant = (searchRestaurant, searchOn) => {};
+
+componentWillMount() {
+
+  let data = null;
+  let xhr = new XMLHttpRequest();
+  let that = this;
+  xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+          console.log(this.responseText);
+          that.setState({
+              restaurantList: JSON.parse(this.responseText).restaurants
+          });
+      }
+  });
+
+  xhr.open("GET", this.props.baseUrl + "restaurant");
+  xhr.setRequestHeader("Cache-Control", "no-cache");
+  xhr.send(data);
+
+}
+
+  updateSearchRestaurant = (searchRestaurant, searchOn) => { };
 
   render() {
     return (
@@ -17,7 +48,10 @@ class Home extends Component {
           baseUrl={this.props.baseUrl}
           updateSearchRestaurant={this.updateSearchRestaurant}
         />
-      </div>
+
+        
+      </div >
+
     );
   }
 }
